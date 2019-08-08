5bbfed4081668cd2310f8a2737477f04f7b0ecf4
Chayaporn Kaewin <>
2019-08-08 14:57:06 +0700

    Add gitignore

---
+++ Random.Joke/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Random.Joke
+//
+//  Created by Chayaporn Kaewin on 8/8/2562 BE.
+//  Copyright © 2562 Chayaporn Kaewin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
